2d00d6b8862e268d39d4a1c7a3eac41bb0a9f13b
Rianda Dalimunte <59490489+Ryanda1106@users.noreply.github.com>
2025-02-07 15:50:30 +0000

    Uploads new file:   .gitignore new file:   chains/index.js new file:   chains/mainnet/ethereum.js new file:   chains/testnet/sahara.js new file:   chains/testnet/sepolia.js new file:   chains/utils/chainTemplate.js new file:   chains/utils/etc.js new file:   log.txt new file:   package.json new file:   saharadaily.js

---
+++ saharadaily.js
@@ -0,0 +1,51 @@
+const { ethers } = require('ethers');
+const kleur = require("kleur");
+const axios = require("axios");
+const chains = require('./chains');
+const provider = chains.testnet.sahara.provider();
+const explorer = chains.testnet.sahara.explorer;
+const fs = require('fs');
+const moment = require('moment-timezone');
+const delay = chains.utils.etc.delay;
+const loading = chains.utils.etc.loadingAnimation;
+const header = chains.utils.etc.header;
+const timelog = chains.utils.etc.timelog;
+const countdown = chains.utils.etc.countdown;
+const PRIVATE_KEYS = JSON.parse(fs.readFileSync('privateKeys.json', 'utf-8'));
+const path = require("path");
+
+function appendLog(message) {
+  fs.appendFileSync('log.txt', message + '\n');
+}
+
+async function dailyTransaction(privateKey) {
+    const wallet = new ethers.Wallet(privateKey, provider);
+	await loading(`Start Transaction for Wallet ${wallet.address}...`, 2000);
+    const tx = {
+        to: wallet.address,
+        value: ethers.parseEther("0.0001"),
+    };
+    try {
+        const signedTx = await wallet.sendTransaction(tx);
+        const receipt = await signedTx.wait(1);
+		const successMesssage = `[${timelog()}] Transaction Confirmed: ${explorer.tx(receipt.hash)}`
+        console.log(kleur.green(successMesssage));
+		appendLog(successMesssage)
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+async function runTransaction() {
+    header();
+    for (const [index, privateKey] of PRIVATE_KEYS.entries()) {
+        try {
+            await dailyTransaction(privateKey);
+            console.log('');
+        } catch (error) {
+            const errorMesssage =`[${timelog()}] Error processing wallet ${index + 1}: ${error.message}`;
+			console.log(kleur.green(errorMessage));
+			appendLog(errorMessage);
+        }
+    }
+}
+runTransaction();
